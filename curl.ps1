50f7b7b7a86e7d3645096c916e0417116c727054
David VelaT <david.vela.tirado@gmail.com>
2020-10-30 00:57:00 +0100

    curl test in pws

---
+++ curl.ps1
@@ -0,0 +1,14 @@
+Get-Command curl
+Get-Command curl.exe
+
+# curl https://1234 -v -k \
+# -H"Content-Type: application/json" \
+# -d '{"file_name":"smiley.PNG","file_mod_time":1505235439292,"file_create_time":1505235439292,"filetransfer_id":"dd7fe41b-b5fd-43de-8a0d-cac4052ee789"}'
+
+# curl https://URL1234 -v 
+# -H "Content-Type: application/json" 
+# -d '{"file_name":"smiley.PNG","file_mod_time":1505235439292,"file_create_time":1505235439292,"filetransfer_id":"dd7fe41b-b5fd-43de-8a0d-cac4052ee789"}'
+
+
+# curl https://curl.haxx.se
+curl   "https://en.wikipedia.org/wiki/List_of_Olympic_medalists_in_judo?action=raw" -v
